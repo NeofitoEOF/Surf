c7fd37a4079edf557e8b0e4c1218bac2663246c5
NeofitoEOF <lorenconi16@gmail.com>
2023-09-07 17:04:48 -0300

    feat: add primeiro caso de teste, configuracao setupserver

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import { Server } from '@overnightjs/core';
+import './util/module-alias';
+import bodyParser from 'body-parser';
+import { ForecastController } from './controller/forecast';
+import { Application } from 'express-serve-static-core';
+
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
+
+  public init(): void {
+    this.setupExpress();
+    this.setupController();
+  }
+
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
+
+  private setupController(): void {
+    const forecastController = new ForecastController();
+    this.addControllers(forecastController);
+  }
+
+
+  public getApp(): Application {
+    return this.app;
+  }
+}

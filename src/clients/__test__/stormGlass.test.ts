0dde074d7c16267270055a981a3f21426470db8f
NeofitoEOF <lorenconi16@gmail.com>
2023-09-07 21:06:43 -0300

    test: add case test stormGlass

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,13 +1,20 @@
-import { StormGlass } from "@src/clients/stormGlass";
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+import * as stormglassWeatherPointFixture from '@test/fixtures/stormglass_weather_3_hours.json';
 
+jest.mock('axios');
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
   it('Should return the normalized forecast from the StormGlass service', async () => {
-      const lat = -33.792726;
-      const lng = 151.289824;
-      
-      const stormGlass = new StormGlass();
-      const response = await stormGlass.fetchPoints(lat, lng);
-      expect(response).toEqual({});
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    mockedAxios.get.mockResolvedValue({ data: stormglassWeatherPointFixture });
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormGlassWeather3HoursFixture);
   });
 });
